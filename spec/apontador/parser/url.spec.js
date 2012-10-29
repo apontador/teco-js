0072f53dbed832059390ea1a2e6bc254dc0065d0
Eher <alexandre.eher@apontador.com>
2012-10-29 11:57:13 -0200

    Altera o urlParser para trabalhar com requireJs

--- spec/apontador/parser/url.spec.js
+++ spec/apontador/parser/url.spec.js
@@ -1,36 +1,38 @@
 /*global describe:true, it:true, expect:true, beforeEach:true*/
-'use strict';
-describe("Url Parser",  function () {
-    describe("with a poi url",  function () {
-        var poi, pathname;
-        beforeEach(function () {
-            pathname = "/local/pr/campo_largo/automoveis_e_veiculos/222Z836E/concessionaria_gm___spack__oa_.html";
-            poi = Apt.urlParser.poi(pathname);
+require(['urlParser'], function (urlParser) {
+    'use strict';
+    describe("Url Parser",  function () {
+        describe("with a poi url",  function () {
+            var poi, pathname;
+            beforeEach(function () {
+                pathname = "/local/pr/campo_largo/automoveis_e_veiculos/222Z836E/concessionaria_gm___spack__oa_.html";
+                poi = urlParser.poi(pathname);
+            });
+            it("should get poi state", function () {
+                expect(poi.state).toBe('pr');
+            });
+            it("should get poi city", function () {
+                expect(poi.city).toBe('campo largo');
+            });
+            it("should get poi name", function () {
+                expect(poi.name).toBe('concessionaria gm spack oa');
+            });
         });
-        it("should get poi state", function () {
-            expect(poi.state).toBe('pr');
-        });
-        it("should get poi city", function () {
-            expect(poi.city).toBe('campo largo');
-        });
-        it("should get poi name", function () {
-            expect(poi.name).toBe('concessionaria gm spack oa');
-        });
-    });
-    describe("with an invalid url",  function () {
-        var poi, pathname;
-        beforeEach(function () {
-            pathname = "/local/search.html";
-            poi = Apt.urlParser.poi(pathname);
-        });
-        it("should get empty poi state", function () {
-            expect(poi.state).toBe('');
-        });
-        it("should get empty poi city", function () {
-            expect(poi.city).toBe('');
-        });
-        it("should get empty poi name", function () {
-            expect(poi.name).toBe('');
+        describe("with an invalid url",  function () {
+            var poi, pathname;
+            beforeEach(function () {
+                pathname = "/local/search.html";
+                poi = urlParser.poi(pathname);
+            });
+            it("should get empty poi state", function () {
+                expect(poi.state).toBe('');
+            });
+            it("should get empty poi city", function () {
+                expect(poi.city).toBe('');
+            });
+            it("should get empty poi name", function () {
+                expect(poi.name).toBe('');
+            });
         });
     });
 });
--- src/apontador/parser/url.js
+++ src/apontador/parser/url.js
@@ -1,37 +1,36 @@
-'use strict';
-var Apt = (function () {
-    return {
-        urlParser: (function () {
-            var poi, isPoiPathname;
-            poi = function (pathname) {
-                var poi_properties, name = "", city = "", state = "";
-                poi_properties = pathname.match(
-                    /* http://rubular.com/r/8DbITZaScI */
-                    /\/local\/(\w{2})\/(\w+)\/\w*?\/\w*?\/(\w+)\.html/
-                );
+define(
+    'urlParser',
+    function () {
+        'use strict';
+        var poi, isPoiPathname;
+        poi = function (pathname) {
+            var poi_properties, name = "", city = "", state = "";
+            poi_properties = pathname.match(
+                /* http://rubular.com/r/8DbITZaScI */
+                /\/local\/(\w{2})\/(\w+)\/\w*?\/\w*?\/(\w+)\.html/
+            );
 
-                if (isPoiPathname(pathname)) {
-                    /* http://rubular.com/r/vC6tWpm1yW */
-                    name = poi_properties[3].replace(/(_+)/g, " ").trim();
-                    city = poi_properties[2].replace("_", " ");
-                    state = poi_properties[1];
-                }
+            if (isPoiPathname(pathname)) {
+                /* http://rubular.com/r/vC6tWpm1yW */
+                name = poi_properties[3].replace(/(_+)/g, " ").trim();
+                city = poi_properties[2].replace("_", " ");
+                state = poi_properties[1];
+            }
 
-                return {
-                    name: name,
-                    city: city,
-                    state: state
-                };
-            };
-            isPoiPathname = function (pathname) {
-                /* http://rubular.com/r/Ad2jwnQbgz */
-                return pathname.match(
-                    (/\/local\/\w{2}\/\w+\/\w*?\/\w*?\/\w+\.html/)
-                );
-            };
             return {
-                poi: poi
+                name: name,
+                city: city,
+                state: state
             };
-        }())
-    };
-}());
+        };
+        isPoiPathname = function (pathname) {
+            /* http://rubular.com/r/Ad2jwnQbgz */
+            return pathname.match(
+                (/\/local\/\w{2}\/\w+\/\w*?\/\w*?\/\w+\.html/)
+            );
+        };
+        return {
+            poi: poi
+        };
+    }
+);
