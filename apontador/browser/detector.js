6ca1ed15a1c6890df266d7596c4ae15a0030c5e9
Renan Ivo <renanivom@gmail.com>
2012-11-12 16:40:04 -0200

    import mobile browser detector as is

---
+++ apontador/browser/detector.js
@@ -0,0 +1,6 @@
+Detector = function(agent) {
+    this.agent = agent || navigator.userAgent || "";
+};
+Detector.prototype.isMobile = function() {
+    return this.agent.match(/(iPhone|iPod|Android|BlackBerry|Windows Phone (OS )?[1-9])/) !== null;
+};
