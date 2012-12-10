df087012077ddb923902a6f16ca734ab44af2d40
guhelski <guilherme.uhelski@apontador.com>
2012-12-10 15:31:22 -0200

    Refactoring

--- apontador/string/mask.js
+++ apontador/string/mask.js
@@ -6,9 +6,6 @@ define(
                 if (phonenumber.match(/^0[1-9]00/) != null) {
                     return '(9999) 999-9999';
                 }
-                if (phonenumber.length == 11) {
-                    return '(99) 9999-9999';
-                }
                 return '(99) 99999-9999';
             }
         };
