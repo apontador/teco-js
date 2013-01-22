037e82899107312008d137583fce8807343958c3
Helder Santana <helder.css@gmail.com>
2013-01-22 17:57:37 -0200

    fix ie7 placeholder attribute bug

--- apontador/polyfill/placeholder.js
+++ apontador/polyfill/placeholder.js
@@ -9,19 +9,19 @@ define(['../browser/event'], function (eventEmitter) {
             for (var i=0, count = inputs.length; i<count; i++) {
                 elm = inputs[i];
 
-                if ( elm.getAttribute('placeholder') ) {
+                if ( elm.attributes.placeholder.value ) {
 
                     if(elm.value === '') {
                         elm.className += clsPrefix;
-                        elm.value = elm.getAttribute("placeholder");
+                        elm.value = elm.attributes.placeholder.value;
                     }
 
-                    eventEmitter.add('click', elm, function (e) {
+                    eventEmitter.add('focus', elm, function (e) {
 
                         //ie8 and ie7 doesn't support this
                         var current = e.target;
 
-                        if (current.value === current.getAttribute('placeholder')) {
+                        if (current.value === current.attributes.placeholder.value) {
                             current.value = '';
                             current.className = current.className.replace('placeholder-on','');
                         }
@@ -33,7 +33,7 @@ define(['../browser/event'], function (eventEmitter) {
                         var current = e.target;
 
                         if (current.value === '') {
-                            current.value = current.getAttribute("placeholder");
+                            current.value = current.attributes.placeholder.value;
 
                             if (!current.className.match('placeholder')) {
                                 current.className += clsPrefix;
--- spec/apontador/polyfill/placeholder.spec.js
+++ spec/apontador/polyfill/placeholder.spec.js
@@ -12,34 +12,34 @@ require(['apontador/polyfill/placeholder','apontador/browser/event'], function(p
 
         it('should input have same value of placeholder attribute', function () {
             expect(input.value).to.not.be.empty();
-            expect(input.value).to.eql(input.getAttribute('placeholder'));
+            expect(input.value).to.eql(input.attributes.placeholder.value);
         });
 
         it('should not put placeholder when the input is filled', function () {
             var expected = 'foobar';
 
-            eventEmitter.emit('click',input);
+            eventEmitter.emit('focus',input);
             input.value = expected;
             eventEmitter.emit('blur',input);
 
-            expect(input.value).not.to.equal(input.getAttribute('placeholder'));
+            expect(input.value).not.to.equal(input.attributes.placeholder.value);
             expect(input.value).to.eql(expected);
         });
 
         it('should insert placeholder when input value is empty', function () {
 
-            eventEmitter.emit('click',input);
+            eventEmitter.emit('focus',input);
             input.value = '';
             eventEmitter.emit('blur',input);
 
             expect(input.value).to.not.be.empty();
-            expect(input.value).to.eql(input.getAttribute('placeholder'));
+            expect(input.value).to.eql(input.attributes.placeholder.value);
         });
 
 
         it('should remove placeholder css class when a input is selected', function () {
 
-            eventEmitter.emit('click',input);
+            eventEmitter.emit('focus',input);
 
             expect(input.className).to.not.contain('placeholder-on');
         });
