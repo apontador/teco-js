5876b7c7ecadd008190ca135d2495a5a1761da57
Helder Santana <helder.css@gmail.com>
2013-01-22 19:02:12 -0200

    add event target test

--- spec/apontador/browser/event.spec.js
+++ spec/apontador/browser/event.spec.js
@@ -6,12 +6,17 @@ require(['apontador/browser/event'], function (eventEmitter) {
             evtType = 'change',
             callback = sinon.spy();
 
-        it("should run an event with callback", function () {
+        before(function(){
             eventEmitter.add(evtType, input, callback);
             eventEmitter.emit(evtType, input);
+        });
 
+        it("should run an event with callback", function () {
             expect(callback.callCount).to.eql(1);
+        });
 
+        it("should have event.target for all browsers", function () {
+            expect(callback.getCall(0).args[0]).to.have.property('target', input);
         });
 
     });
