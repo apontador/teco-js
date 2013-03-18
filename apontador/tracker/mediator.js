fb24f0ab0a2eb273d8c19434adbd4e1f56223f70
Renan Ivo <renan.martins@apontador.com>
2013-03-18 20:11:41 -0300

    remove jQuery from mediator dependencies
    to figure out why r.js can't compile it

--- apontador/tracker/mediator.js
+++ apontador/tracker/mediator.js
@@ -1,6 +1,6 @@
 /*global define, jQuery*/
 define(
-    ['apontador/tracker/event/type/dispatcher', 'jquery'],
+    ['apontador/tracker/event/type/dispatcher'],
     function (typeDispatcher) {
         'use strict';
 
