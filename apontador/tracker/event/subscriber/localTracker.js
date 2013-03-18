a0850f714f977a1f4e35a93fc3f8361b0608930d
Renan Ivo <renan.martins@apontador.com>
2013-03-18 17:15:20 -0300

    create localTracker event subscriber to Tracker Mediator

---
+++ apontador/tracker/event/subscriber/localTracker.js
@@ -0,0 +1,28 @@
+/*global localTracker, define, poiId*/
+define(function () {
+    'use strict';
+
+    return function (type, name, attribues) {
+        var event_dictionary = {
+            'view': {
+                'utility': 'SHOW_UTILITY',
+            },
+            'click': {
+                'phone': 'SEE_PHONE',
+                'site': 'GO_TO_SITE',
+                'email': 'SHARE_EMAIL',
+                'description': 'DESCRIPTION',
+                'facebook_button': 'SHARE_FACEBOOK',
+                'twitter_button': 'SHARE_TWITTER',
+                'thumbs_up': 'THUMBS_UP',
+                'thumbs_down': 'THUMBS_DOWN',
+                'photo_upload': 'SEND_PHOTO',
+                'helpful_review': 'HELPFUL_REVIEW',
+                'utility': 'CLICK_UTILITY',
+                'embed_map': 'MAPLINK_WIDGET'
+            }
+        };
+
+        localTracker.trackEvent(event_dictionary[type][name], poiId);
+    };
+});
---
+++ spec/apontador/tracker/event/subscriber/localTracker.spec.js
@@ -0,0 +1,35 @@
+/*global require, describe, it, expect, beforeEach, afterEach, sinon, window*/
+require(
+    ['apontador/tracker/event/subscriber/localTracker'],
+    function (localTrackerSub) {
+        'use strict';
+
+        describe(
+            "Tracker Event Subscriber: LocalTracker",
+            function () {
+
+                beforeEach(function () {
+                    window.localTracker = {
+                        trackEvent: sinon.spy()
+                    };
+                    window.poiId = 'LBSID';
+                });
+
+                afterEach(function () {
+                    delete window.localTracker;
+                    delete window.poiId;
+                });
+
+                it("should call the localTracker with the correct event", function () {
+                    localTrackerSub('view', 'utility');
+
+                    expect(window.localTracker.trackEvent.calledOnce).to.be.ok();
+                    expect(window.localTracker.trackEvent.getCall(0).args).to.eql([
+                        'SHOW_UTILITY',
+                        'LBSID'
+                    ]);
+                });
+            }
+        );
+    }
+);
