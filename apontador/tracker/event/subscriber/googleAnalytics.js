571db4fa77b5b01774d8f7ae66dc4badfe33eb4f
Renan Ivo <renan.martins@apontador.com>
2013-03-26 09:59:09 -0300

    add google analytics tracker event subscriber

---
+++ apontador/tracker/event/subscriber/googleAnalytics.js
@@ -0,0 +1,25 @@
+/*global define*/
+define(function () {
+    'use strict';
+
+    return function (type, name, attributes) {
+        if (type !== 'click') {
+            return;
+        }
+
+        switch (name) {
+        case 'place_phone':
+            pageTracker._trackPageview(
+                '/contabilizacao/poi/' + attributes.place_id
+                    + '/' + attributes.name + '/vertelefone.html'
+            );
+            break;
+        case 'place_website_link':
+            pageTracker._trackPageview(
+                '/contabilizacao/poi/' + attributes.place_id
+                    + '/Mais_informacoes.html'
+            );
+            break;
+        }
+    };
+});
---
+++ spec/apontador/tracker/event/subscriber/googleAnalytics.spec.js
@@ -0,0 +1,79 @@
+/*global require, describe, it, beforeEach, afterEach, sinon, expect, window*/
+require(
+    ['apontador/tracker/event/subscriber/googleAnalytics'],
+    function (googleAnalyticsSub) {
+        'use strict';
+
+        describe('Tracker Event subscriber: Google Analytics', function () {
+            beforeEach(function () {
+                window.pageTracker = {
+                    '_trackPageview': sinon.spy()
+                };
+            });
+            afterEach(function () {
+                delete window.pageTracker;
+            });
+
+            it("should track a phone click", function () {
+                googleAnalyticsSub(
+                    'click',
+                    'place_phone',
+                    {
+                        'place_id': 'LBSID',
+                        'name': 'Foo Pub'
+                    }
+                );
+
+                expect(window.pageTracker._trackPageview.calledOnce)
+                    .to.be.ok();
+                expect(window.pageTracker._trackPageview.firstCall.args)
+                    .to.eql([
+                        '/contabilizacao/poi/LBSID/Foo Pub/vertelefone.html'
+                    ]);
+            });
+
+            it('should track a click on the place website link', function () {
+                googleAnalyticsSub(
+                    'click',
+                    'place_website_link',
+                    {
+                        'place_id': 'LBSID',
+                    }
+                );
+
+                expect(window.pageTracker._trackPageview.calledOnce)
+                    .to.be.ok();
+                expect(window.pageTracker._trackPageview.firstCall.args)
+                    .to.eql([
+                        '/contabilizacao/poi/LBSID/Mais_informacoes.html'
+                    ]);
+            });
+
+            it('should not track an unknown event', function () {
+                googleAnalyticsSub(
+                    'click',
+                    'unknown',
+                    {
+                        'place_id': 'LBSID',
+                    }
+                );
+
+                expect(window.pageTracker._trackPageview.callCount)
+                    .to.eql(0);
+            });
+
+            it('should not track unsupported event types', function () {
+                googleAnalyticsSub(
+                    'view',
+                    'place_phone',
+                    {
+                        'place_id': 'LBSID',
+                    }
+                );
+
+                expect(window.pageTracker._trackPageview.callCount)
+                    .to.eql(0);
+            });
+        });
+    }
+);
