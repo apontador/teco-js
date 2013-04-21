7623bbcb7ac3e3ae6f7402f4dc54db8f6df11e19
Renan Ivo <renanivom@gmail.com>
2013-04-20 23:21:58 -0300

    add grunt jshint task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,35 @@
-module.exports = function(grunt) {
+/*jshint scripturl:true*/
+module.exports = function (grunt) {
+    "use strict";
 
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         mocha: {
-            all: {
+            test: {
+                src: ['spec/SpecRunner.html'],
+                options: {
+                    run: false,
+                    reporter: 'Dot'
+                }
+            },
+            spec: {
                 src: ['spec/SpecRunner.html'],
                 options: {
                     run: false,
                     reporter: 'Spec'
                 }
             }
+        },
+        jshint: {
+            all: ['Gruntfile.js', 'apontador/**/*.js', 'spec/**/*.js'],
+            jshintrc: '.jshintrc'
         }
     });
 
     grunt.loadNpmTasks('grunt-mocha');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
 
     // Default task(s).
-    grunt.registerTask('default', ['mocha']);
+    grunt.registerTask('default', ['mocha:spec']);
 };
