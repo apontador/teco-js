4970362de6ed8d88710ae0c01a9e7fe88f0a4e88
Rafael Lyra <rafael@rafaellyra.com>
2013-11-12 14:51:49 -0200

    Change title var name to newtitle on history polyfill

--- apontador/polyfill/history.js
+++ apontador/polyfill/history.js
@@ -2,13 +2,13 @@ define(
     function () {
         'use strict';
         function changeHash(value, title) {
-            var title = title || document.title;
+            var newtitle = title || document.title;
             if (history.pushState) {
                 history.pushState(null, null, value);
             } else {
                 location.hash = value;
             }
-            document.title = title;
+            document.title = newtitle;
         }
         return changeHash;
     }
